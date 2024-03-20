a7c4777840ca0f9c678233ae2d4b6df26295ff0b
daria <darias2002@gmail.com>
2024-03-20 18:45:45 +0000

    questions inquirer

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,32 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What's the team manager's name?",
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What's the team manager's ID?",
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What's the team manager's email?",
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What's the team manager's office number?",
+    },
+    //stopping here in case they pick "none"
+    {
+        type: "list",
+        name: "addMember",
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "None"]
+    },
+]
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -4,6 +4,12 @@ class Manager extends Employee {
         super (name, id, email);
         this.officeNumber = officeNumber;
     }
+    getOfficeNumber() {
+        return this.officeNumber
+    }
+    getRole() {
+        return Manager;
+    }
 }
 
 module.exports = Manager;
